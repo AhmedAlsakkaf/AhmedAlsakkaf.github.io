409260abf51860a305c3e0270d8956313b2837cc
Ahmed abdulkareem <ahmedabdalkaremm@gmail.com>
2024-04-09 00:20:35 +0500

    AnimatedTextAdded

--- js/main.js
+++ js/main.js
@@ -1,3 +1,45 @@
+/*=============== Changing Text ===============*/
+
+function sleep(ms) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
+const phrases = ["Software Engineer", "Frontend Developer", "UI/UX Designer", "Backend Developer", "Digital Marketer"];
+const el = document.getElementById("typewriter");
+
+let sleepTime = 60;
+
+let curPhraseIndex = 0;
+
+const writeLoop = async () => {
+  while (true) {
+    let curWord = phrases[curPhraseIndex];
+
+    for (let i = 0; i < curWord.length; i++) {
+      el.innerText = curWord.substring(0, i + 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 10);
+
+    for (let i = curWord.length; i > 0; i--) {
+      el.innerText = curWord.substring(0, i - 1);
+      await sleep(sleepTime);
+    }
+
+    await sleep(sleepTime * 5);
+
+    if (curPhraseIndex === phrases.length - 1) {
+      curPhraseIndex = 0;
+    } else {
+      curPhraseIndex++;
+    }
+  }
+};
+
+writeLoop();
+
+
 /*=============== SHOW SIDEBAR ===============*/
 const navMenu = document.getElementById('sidebar'),
       navToggle = document.getElementById('nav-toggle'),
